a29290ad4db1e2cf3adae574ef1bcc027c65ad08
Alessandro Dias Jr <alessandro.dsjr@gmail.com>
2021-09-02 23:54:25 -0300

    Estilazação da pagina inicial

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -16,7 +16,7 @@ export class InicioComponent implements OnInit {
   ngOnInit(): void {
 
     if(environment.token == ''){
-      alert('Sua seção expirou, faça o login novamente.')
+
       this.router.navigate(['/entrar']);
     }
   }
