0e6f8904dd319c25c2fc8a99f5b0e93f91f8df92
Alessandro Dias Jr <alessandro.dsjr@gmail.com>
2021-09-06 18:39:21 -0300

    Finalização do crud de tema

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -21,8 +21,20 @@ export class TemaService {
       return this.http.get<Tema[]>('https://blogalessandrobe.herokuapp.com/temas', this.token)
     }
 
+    getByIdTema(id: number): Observable<Tema>{
+      return this.http.get<Tema>(`https://blogalessandrobe.herokuapp.com/temas/${id}`, this.token)
+    }
+
     postTema(tema: Tema): Observable<Tema>{
       return this.http.post<Tema>('https://blogalessandrobe.herokuapp.com/temas', tema, this.token)
+    }
+
+    putTema(tema: Tema): Observable<Tema>{
+      return this.http.put<Tema>('https://blogalessandrobe.herokuapp.com/temas', tema, this.token)
+    }
+
+    deleteTema(id: number){
+      return this.http.delete<Tema>(`https://blogalessandrobe.herokuapp.com/temas/${id}`, this.token)
     }
 
 }
